0f80a2621bdf36e8ae5b989aabb2db7a6dfa1651
ogg1996 <rjsrnr43@naver.com>
2024-10-22 15:29:27 +0900

    리액트 기본 생성 파일

---
+++ src/main.jsx
@@ -0,0 +1,53 @@
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
+import { Provider } from 'react-redux';
+import App from './App';
+import './styles/reset.css';
+import { store } from './redux';
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Pretendard-medium';
+    src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 600;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'Cafe24Supermagic-Bold';
+    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_2307-2@1.0/Cafe24Supermagic-Bold-v1.0.woff2') format('woff2');
+    font-weight: 700;
+    font-style: normal;
+  }
+
+  * {
+    box-sizing: border-box;
+    -webkit-user-select:none;
+    -moz-user-select:none;
+    -ms-user-select:none;
+    user-select:none;
+  }
+  
+  html {
+    width: 100%;
+    margin: 0 auto;
+  }
+  body {
+    width: 100%;
+    background-color: #fefbf8;
+  }
+  #root{
+    width: 100%;
+    font-family: 'Pretendard-medium','arial';
+  }
+
+`;
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <GlobalStyle />
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
