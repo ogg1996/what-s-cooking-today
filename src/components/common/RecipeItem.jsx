14a9ceae7139f4890656aeeae13157b59a83c770
ogg1996 <rjsrnr43@naver.com>
2024-10-22 16:07:41 +0900

    레시피 아이템 컴포넌트 / Recommend, RecipeItems에서 사용

---
+++ src/components/common/RecipeItem.jsx
@@ -0,0 +1,88 @@
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledRecipeItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  padding: 10px;
+  cursor: pointer;
+
+  & > div {
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    gap: 3px;
+
+    width: 200px;
+    background-color: #000000;
+    background-image: url(${props => props.$setUrl});
+    background-size: cover;
+    aspect-ratio: 1;
+    border-radius: 8px;
+
+    & > span {
+      display: none;
+      color: white;
+      text-align: right;
+      font-size: 18px;
+    }
+    & > span:first-child {
+      text-align: justify;
+      flex-grow: 1;
+    }
+  }
+
+  & > span {
+    font-size: 20px;
+  }
+
+  &:active,
+  &:hover {
+    & > div {
+      background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+        url(${props => props.$setUrl});
+      & > span {
+        display: inline;
+      }
+    }
+  }
+
+  @media (max-width: 461px) {
+    & > div {
+      width: 165px;
+
+      & > span {
+        font-size: 16px;
+      }
+    }
+    & > span {
+      font-size: 16px;
+    }
+  }
+`;
+
+export default function RecipeItem({ itemData }) {
+  const navigate = useNavigate();
+  return (
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    <>
+      {itemData && (
+        <StyledRecipeItem
+          onClick={() => navigate(`/detail/${itemData.RECIPE_ID}`)}
+          $setUrl={itemData.IMG_URL}
+        >
+          <div>
+            <span>{itemData.SUMRY}</span>
+            <span>분류: {itemData.TYPE}</span>
+            <span>난이도: {itemData.LEVEL}</span>
+            <span>조리시간: {itemData.COOKING_TIME}</span>
+          </div>
+          <span>{itemData.NAME}</span>
+        </StyledRecipeItem>
+      )}
+    </>
+  );
+}
