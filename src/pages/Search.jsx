d94c8a9ffc5938e7a797a4dcb2e09d0752514f72
ogg1996 <rjsrnr43@naver.com>
2024-10-22 15:47:40 +0900

    Search Page: 검색 결과를 보여줄 페이지

---
+++ src/pages/Search.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import styled from 'styled-components';
+import { useDispatch } from 'react-redux';
+import { useAxiosData } from '../hooks/useAxiosData';
+import RecipeItems from '../components/common/RecipeItems';
+import { setPageState } from '../redux';
+import SkeletonRecipeItems from '../skeletons/common/SkeletonRecipeItems';
+
+const StyledSearch = styled.div`
+  font-size: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 30px;
+`;
+
+export default function Search() {
+  const dispatch = useDispatch();
+
+  const [recipeItems, setRecipeItems] = useState(null);
+
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get('query');
+  const { VITE_DB_URL } = import.meta.env;
+
+  useEffect(() => {
+    dispatch(setPageState('search'));
+  }, []);
+
+  useEffect(() => {
+    setRecipeItems(null);
+    useAxiosData(`${VITE_DB_URL}/basic?NAME_like=${query}`).then(res => {
+      const resData = res.data;
+      setRecipeItems(resData);
+    });
+  }, [searchParams]);
+
+  return (
+    <StyledSearch>
+      <p>&apos;{query}&apos;로 검색한 결과 입니다.</p>
+      {recipeItems ? (
+        <RecipeItems recipeItems={recipeItems} />
+      ) : (
+        <SkeletonRecipeItems />
+      )}
+    </StyledSearch>
+  );
+}
