1bbfeb081c79bf3f8955106f07c10c80ff49eb86
ogg1996 <rjsrnr43@naver.com>
2024-10-22 16:04:17 +0900

    하단 네비게이션 컴포넌트

---
+++ src/components/app/BottomNav.jsx
@@ -0,0 +1,74 @@
+import styled from 'styled-components';
+import defaultListIcon from '@assets/images/icons/icon-list-default.png';
+import defaultRecommendIcon from '@assets/images/icons/icon-recommend-default.png';
+import activeListIcon from '@assets/images/icons/icon-list-active.png';
+import activeRecommendIcon from '@assets/images/icons/icon-recommend-active.png';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const StyledBottomNav = styled.nav`
+  display: none;
+
+  @media (max-width: 1000px) {
+    display: flex;
+
+    z-index: 999;
+    position: fixed;
+    bottom: 0px;
+
+    width: 100%;
+    height: 52px;
+    padding: 0 16px;
+
+    background-color: #f6f1eb;
+    border-top: 1px solid #c4c4c4;
+
+    & > a {
+      flex-grow: 1;
+
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+
+      img {
+        width: 24px;
+        height: 24px;
+      }
+    }
+  }
+`;
+const ButtomNavItem = styled.span`
+  font-size: 12px;
+  color: ${({ $active }) => ($active ? '#e74c3c' : '#99806c;')};
+`;
+
+export default function BottomNav() {
+  const pageState = useSelector(state => state.pageState.page);
+  return (
+    <StyledBottomNav>
+      <Link to="/recommend">
+        <img
+          src={
+            pageState === 'recommend'
+              ? activeRecommendIcon
+              : defaultRecommendIcon
+          }
+          alt="아이콘"
+        />
+        <ButtomNavItem $active={pageState === 'recommend'}>
+          메뉴 추천
+        </ButtomNavItem>
+      </Link>
+      <Link to="/recipeList">
+        <img
+          src={pageState === 'recipeList' ? activeListIcon : defaultListIcon}
+          alt="아이콘"
+        />
+        <ButtomNavItem $active={pageState === 'recipeList'}>
+          레시피 목록
+        </ButtomNavItem>
+      </Link>
+    </StyledBottomNav>
+  );
+}
