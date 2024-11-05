12e3d0a32cd46cba6f75ee0ef2269dc330972451
ogg1996 <rjsrnr43@naver.com>
2024-11-05 20:09:07 +0900

    #16 feature/누락된 코드 커밋..

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -6,7 +6,7 @@ import RecipeItems from '@components-common/RecipeItems';
 import SkeletonRecipeItems from '@skeletons-common/SkeletonRecipeItems';
 import { setPageState } from '@/redux';
 import { useAxiosData } from '@/hooks/useAxiosData';
-import { useScrollTop } from '@/hooks/useScrollToY';
+import { useScrollToY } from '@/hooks/useScrollToY';
 
 const StyledSearch = styled.div`
   font-size: 20px;
@@ -26,7 +26,7 @@ export default function Search() {
   const { VITE_DB_URL } = import.meta.env;
 
   useEffect(() => {
-    useScrollTop();
+    useScrollToY(0);
     dispatch(setPageState('search'));
   }, []);
 
