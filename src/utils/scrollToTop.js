3cdb2c744d4519710a4b95c5021b74c85d6bf3cf
ogg1996 <9migoAhri@gmail.com>
2025-01-16 14:12:18 +0900

    #46 perf: behavior를 인자로 받도록 수정

--- src/utils/scrollToTop.js
+++ src/utils/scrollToTop.js
@@ -1,5 +1,5 @@
-const scrollToTop = () => {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
+const scrollToTop = (behavior = 'auto', top = 0) => {
+  window.scrollTo({ top, behavior });
 };
 
 export default scrollToTop;
